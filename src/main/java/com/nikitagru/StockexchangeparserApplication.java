86a92f3900ee6d489483c1758fe9a671c502f307
nikitagru <grunix123@gmail.com>
2021-06-18 23:03:20 +0500

    Initial commit

---
+++ src/main/java/com/nikitagru/StockexchangeparserApplication.java
@@ -0,0 +1,13 @@
+package com.nikitagru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockexchangeparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockexchangeparserApplication.class, args);
+	}
+
+}
